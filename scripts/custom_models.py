8f3c4c47ca79dda024c50b01fa68ebc48eaf2eba
Chris Hamblin <chrishamblin7@gmail.com>
2020-08-16 11:21:15 -0400

    branching net

--- scripts/custom_models.py
+++ scripts/custom_models.py
@@ -39,3 +39,40 @@ class CustomNet_small(torch.nn.Module):
 		x = torch.flatten(x, 1)
 		x = self.classifier(x)
 		return x
+
+
+
+
+class BranchingNet(torch.nn.Module):
+	def __init__(self, out_channels = 26, in_channels = 3):
+		super(BranchingNet, self).__init__()
+		self.out_channels = out_channels
+		self.in_channels = in_channels
+
+		self.features = nn.Sequential(
+			nn.Conv2d(self.in_channels, 40, kernel_size=(5, 5), stride=(2, 2), padding=(3, 3), bias=False),
+			nn.ReLU(inplace=True),
+			#nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+			nn.Conv2d(40, 20, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(20, 60, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+			nn.ReLU(inplace=True))
+
+		self.avgpool = nn.AdaptiveAvgPool2d((4, 4))
+
+		self.classifier = nn.Sequential(
+			nn.Dropout(),
+			nn.Linear(60*4*4, 500),
+			nn.ReLU(inplace=True),
+			nn.Dropout(),
+			nn.Linear(500, 200),
+			nn.ReLU(inplace=True),
+			nn.Linear(200, self.out_channels))
+
+
+	def forward(self, x):
+		x = self.features(x)
+		x = self.avgpool(x)
+		x = torch.flatten(x, 1)
+		x = self.classifier(x)
+		return x
--- scripts/get_test_activations.py
+++ scripts/get_test_activations.py
@@ -11,13 +11,45 @@ import pickle
 SUM = True  #sum output activation maps
 FLIP = True #flip output so its featuresXimages not imagesXfeatures
 output_type = 'mat'
-output_names = ['alexnet_letter_aug_seed0_acc0.981','alexnet_letter_aug_seed1_acc0.988',
-				'alexnet_letter_aug_seed2_acc0.982','alexnet_letter_aug_seed3_acc0.989','alexnet_letter_aug_seed4_acc0.978',
-				'alexnet_letter_aug_seed5_acc0.979','alexnet_letter_aug_seed6_acc0.987','alexnet_letter_aug_seed7_acc0.992',
-				'alexnet_letter_aug_seed8_acc0.991','alexnet_letter_aug_seed9_acc0.989','customsmall_letter_aug_seed0_acc0.964',
-				'customsmall_letter_aug_seed1_acc0.973','customsmall_letter_aug_seed2_acc0.955','customsmall_letter_aug_seed3_acc0.966',
-				'customsmall_letter_aug_seed4_acc0.963','customsmall_letter_aug_seed5_acc0.968','customsmall_letter_aug_seed6_acc0.968',
-				'customsmall_letter_aug_seed7_acc0.973','customsmall_letter_aug_seed8_acc0.971','customsmall_letter_aug_seed9_acc0.965']
+#output_names = ['alexnet_letter_aug_seed0_acc0.981','alexnet_letter_aug_seed1_acc0.988',
+#				'alexnet_letter_aug_seed2_acc0.982','alexnet_letter_aug_seed3_acc0.989','alexnet_letter_aug_seed4_acc0.978',
+#				'alexnet_letter_aug_seed5_acc0.979','alexnet_letter_aug_seed6_acc0.987','alexnet_letter_aug_seed7_acc0.992',
+#				'alexnet_letter_aug_seed8_acc0.991','alexnet_letter_aug_seed9_acc0.989','customsmall_letter_aug_seed0_acc0.964',
+#				'customsmall_letter_aug_seed1_acc0.973','customsmall_letter_aug_seed2_acc0.955','customsmall_letter_aug_seed3_acc0.966',
+#				'customsmall_letter_aug_seed4_acc0.963','customsmall_letter_aug_seed5_acc0.968','customsmall_letter_aug_seed6_acc0.968',
+#				'customsmall_letter_aug_seed7_acc0.973','customsmall_letter_aug_seed8_acc0.971','customsmall_letter_aug_seed9_acc0.965']
+#output_name = 'alexnet_imagenet'
+
+subfolder = 'branchingnet'        #(branchingnet,alexnet,smallnet)
+
+output_names = [
+'branchingnet_relu1_letter_seed0_acc0.968','branchingnet_relu1_letter_seed8_acc0.969','branchingnet_relu2_letter_seed6_acc0.979','branchingnet_relu3_letter_seed4_acc0.983',  
+'branchingnet_relu4_letter_seed2_acc0.972','branchingnet_relu5_letter_seed0_acc0.973','branchingnet_relu5_letter_seed8_acc0.969','branchingnet_relu1_letter_seed1_acc0.971',  
+'branchingnet_relu1_letter_seed9_acc0.963','branchingnet_relu2_letter_seed7_acc0.978','branchingnet_relu3_letter_seed5_acc0.978','branchingnet_relu4_letter_seed3_acc0.974',  
+'branchingnet_relu5_letter_seed1_acc0.972','branchingnet_relu5_letter_seed9_acc0.971','branchingnet_relu1_letter_seed2_acc0.976','branchingnet_relu2_letter_seed0_acc0.977',
+'branchingnet_relu2_letter_seed8_acc0.976','branchingnet_relu3_letter_seed6_acc0.983','branchingnet_relu4_letter_seed4_acc0.975','branchingnet_relu5_letter_seed2_acc0.971',
+'branchingnet_relu1_letter_seed3_acc0.972','branchingnet_relu2_letter_seed1_acc0.975','branchingnet_relu2_letter_seed9_acc0.975','branchingnet_relu3_letter_seed7_acc0.981',  
+'branchingnet_relu4_letter_seed5_acc0.967','branchingnet_relu5_letter_seed3_acc0.967','branchingnet_relu1_letter_seed4_acc0.972','branchingnet_relu2_letter_seed2_acc0.978',  
+'branchingnet_relu3_letter_seed0_acc0.978','branchingnet_relu3_letter_seed8_acc0.981','branchingnet_relu4_letter_seed6_acc0.975','branchingnet_relu5_letter_seed4_acc0.972',
+'branchingnet_relu1_letter_seed5_acc0.967','branchingnet_relu2_letter_seed3_acc0.979','branchingnet_relu3_letter_seed1_acc0.982','branchingnet_relu3_letter_seed9_acc0.98', 
+'branchingnet_relu4_letter_seed7_acc0.973','branchingnet_relu5_letter_seed5_acc0.972','branchingnet_relu1_letter_seed6_acc0.969','branchingnet_relu2_letter_seed4_acc0.977', 
+'branchingnet_relu3_letter_seed2_acc0.983','branchingnet_relu4_letter_seed0_acc0.976','branchingnet_relu4_letter_seed8_acc0.976','branchingnet_relu5_letter_seed6_acc0.973',
+'branchingnet_relu1_letter_seed7_acc0.963','branchingnet_relu2_letter_seed5_acc0.977','branchingnet_relu3_letter_seed3_acc0.982','branchingnet_relu4_letter_seed1_acc0.977',  
+'branchingnet_relu4_letter_seed9_acc0.976','branchingnet_relu5_letter_seed7_acc0.971']
+
+
+if subfolder == 'branchingnet':
+	branch_key = {  #a dict that gives the alexnet module name from the name in the model file
+	'relu1':'1',
+	'relu2':'4',
+	'relu3':'7',
+	'relu4':'9',
+	'relu5':'11'
+	}
+	alexnet = models.alexnet(pretrained=True)
+	alexnet.eval()
+	from train_branching_model import through_network
+
 
 ####END OF PARAMS ########
 
@@ -28,8 +60,7 @@ test_loader = DataLoader(
 
 for output_name in output_names:
 	#model
-	model = torch.load('../models/%s.pt'%output_name)
-	#model = models.alexnet(pretrained=False)
+	model = torch.load('../models/%s/%s.pt'%(subfolder,output_name))
 	model.to('cpu')
 
 	#num_ftrs = model.classifier[6].in_features
@@ -39,7 +70,12 @@ for output_name in output_names:
 	#get activations into dict
 	activations = {}
 	for data,target in test_loader:
+		
+		if subfolder == 'branchingnet':    #we need to pass input through alexnet
+			branch_point = branch_key[output_name.split('_')[1]]
+			data = through_network(data, network=alexnet.features,branch_point = branch_point)
 		x = data
+
 		# get conv features
 		print('features')
 		for layer, (name, module) in enumerate(model.features._modules.items()):
@@ -51,6 +87,7 @@ for output_name in output_names:
 			if FLIP:
 				activation = np.swapaxes(activation,0,1)
 			activations['features'+str(module).split('(')[0]+'_'+str(layer)] = activation
+
 		# get avgpool
 		x = model.avgpool(x)
 		activation = x.detach().numpy()
@@ -61,6 +98,7 @@ for output_name in output_names:
 		activations['avg_pool'] = activation
 		
 		x = torch.flatten(x, 1)
+
 		#classifier
 		print('classifier')
 		for layer, (name, module) in enumerate(model.classifier._modules.items()):
@@ -84,11 +122,11 @@ for output_name in output_names:
 	print(output.shape)
 
 	if output_type == 'mat':
-		scipy.io.savemat('../activations/%s_activations.mat'%output_name, activations)
-		scipy.io.savemat('../activations/%s_output.mat'%output_name, dict(output=output.detach().numpy()))
+		scipy.io.savemat('../activations/%s/%s_activations.mat'%(subfolder,output_name), activations)
+		#scipy.io.savemat('../activations/%s/%s_output.mat'%(subfolder,output_name), dict(output=output.detach().numpy()))
 	else:
-		pickle.dump(activations, open('../activations/%s_activations.pkl'%output_name,'wb'))
-		pickle.dump(output.detach().numpy(), open('../activations/%s_output.pkl'%output_name,'wb'))
+		pickle.dump(activations, open('../activations/%s/%s_activations.pkl'%(subfolder,output_name),'wb'))
+		#pickle.dump(output.detach().numpy(), open('../activations/%s/%s_output.pkl'%(subfolder,output_name),'wb'))
 
 
 
